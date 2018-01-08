38b5436a70f74c0733f589c2dfabdd6fe315ca7c
jhhwang <jhhwang4195@gmail.com>
2018-01-08 09:07:48 +0900

    Add result of 'df -h'

--- bot_test2.py
+++ bot_test2.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*- 
+import os
 from slacker import Slacker
 
 token = 'Your_Bot_Token'
@@ -7,9 +8,11 @@ slack = Slacker(token)
 attachments_dict = dict()
 attachments_dict['pretext'] = "attachments 블록 전에 나타나는 text"
 attachments_dict['title'] = "다른 텍스트 보다 크고 볼드되어서 보이는 title"
-attachments_dict['title_link'] = "jhhwang4195.tistory.dom"
+attachments_dict['title_link'] = "jhhwang4195.tistory.com"
 attachments_dict['fallback'] = "클라이언트에서 노티피케이션에 보이는 텍스트 입니다. attachment 블록에는 나타나지 않습니다"
-attachments_dict['text'] = "본문 텍스트! 5줄이 넘어가면 *show more*로 보이게 됩니다."
+
+p = os.popen('df -h')
+attachments_dict['text'] = p.read()
 attachments_dict['mrkdwn_in'] = ["text", "pretext"]  # 마크다운을 적용시킬 인자들을 선택합니다.
 attachments = [attachments_dict]
 
